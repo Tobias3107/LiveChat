a6589b99134e3935d7fecdc0fda3781890636e27
Tobias3107 <33278260+Tobias3107@users.noreply.github.com>
2019-01-03 14:54:35 +0100

    1.0.0 Upload
    Upload the Website files
    Including Jquery ver 3.1.1

---
+++ livechat.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    $("#startform").submit(function() {
+        name = $("#name").val();
+        if(name == "") {
+            $("#response").html("Name Eingeben LOS!");
+        } else {
+            con = new WebSocket('ws://localhost:4649/LiveSocket');
+            $("#submit").attr("disable", "disable");
+            $("#response").html("Loading...");
+            con.onopen = function () {
+                con.send('name='+name+ ';' + 'openchat;'); // Send the message 'Ping' to the server
+            };
+            $("#main").html('<form id="test" method="post"><div id="ChatBox"></div><input type="text" id="msg">' +
+                        '<input type="hidden" id="name" value="' + name +'">' +
+                        '<input type="submit" id="msgsend" value="Send">');
+            con.onerror = function (error) {
+                console.log('WebSocket Error ' + error);
+
+            };
+
+            // Log messages from the server
+            con.onmessage = function (e) {
+                $("#ChatBox").append(e.data+"<br / >");
+                console.log(e.data);
+            };
+            
+            con.onclose = function (e) {
+                location.reload();
+            };
+
+            $("#response").html("Mhm OK - " + name);
+            $("#test").submit(function() {
+                console.log("name="+$("#name").val() + ";msg="+$("#msg").val());
+                con.send("name="+$("#name").val() + ";msg="+$("#msg").val());
+                return false;
+            });
+            return false;
+        }
+        return false;
+    });
+
+});
